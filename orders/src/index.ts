402780d6a822f15c93fc7183c28816baba16717c
Giulio Vallini <s276003@studenti.polito.it>
2024-02-01 22:27:09 +0100

    Very small change

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -10,7 +10,7 @@ import {
 } from './events/listeners';
 
 const start = async () => {
-  console.log('starting...');
+  console.log('starting....');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
